10f31decd3162f4dfdfcae84704c09846b12c333
dineshnikhil <dineshkumarpokkula@gmail.com>
2023-07-03 06:50:58 +0530

    used the bodyParser and the added the sequelize, sequelize-cli, mysql2 packages.

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 const express = require('express');
+const body_parser = require('body-parser');
+
 // importing the port from environment variable
 const { PORT } = require('./config/serverConfig');
 
 const create_and_start_server = async () => {
 	// create an object of the express
 	const app = express();
+	app.use(body_parser.json());
+	app.use(body_parser.urlencoded({ extended: true }));
 
 	// start the server at the specific port.
 	app.listen(PORT, () => {
